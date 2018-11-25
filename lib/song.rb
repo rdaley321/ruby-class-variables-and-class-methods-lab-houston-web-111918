372ebd2402d17fcabdb277603539a52d8e36f033
Ryan Daley <rdaley321@yahoo.com>
2018-11-25 03:54:10 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,53 @@
+class Song
+  attr_accessor :name, :artist, :genre
+  
+  @@count = 0
+  @@genres = []
+  @@artists = []
+  
+  def initialize(name,artist,genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@genres << genre
+    @@artists << artist
+  end
+  
+  def self.count
+    @@count
+  end
+  
+  def self.artists
+    @@artists.uniq
+  end
+  
+  def self.genres
+    @@genres.uniq
+  end
+  
+  def self.genre_count
+    hash = Hash.new
+    @@genres.each do |item|
+      if hash[item]
+        hash[item] += 1
+      else
+        hash[item] = 1
+      end
+    end
+    hash
+  end
+  
+  def self.artist_count
+    hash = Hash.new
+    @@artists.each do |item|
+      if hash[item]
+        hash[item] += 1
+      else
+        hash[item] = 1
+      end
+    end
+    hash
+  end
+  
+end
