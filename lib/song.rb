5302c0ca9744b44f617943bcc560d6ae0a84ef2c
Ryan Daley <rdaley321@yahoo.com>
2018-11-25 03:48:55 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,16 +2,31 @@ class Song
   attr_accessor :name, :artist, :genre
   
   @@count = 0
+  @@genres = []
+  @@artists = []
   
   def initialize(name,artist,genre)
     @name = name
     @artist = artist
     @genre = genre
     @@count += 1
+    @@genres << genre
+    @@artists << artist
   end
   
   def self.count
     @@count
+  end
+  
+  def self.genre_count
+    hash = Hash.new
+    @@genres.each do |item|
+      if hash[item]
+        hash[item] += 1
+      else
+        hash[item] = 1
+      end
+    end
   end
   
 end
